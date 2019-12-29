9fff361767555ac238df38586194c76d3dc2ee35
Robert Yeagle <robertsyeagle@gmail.com>
2019-12-29 15:13:59 -0800

    Adding C++ program that evaluates if leap year

---
+++ leap_year.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+ 
+  int input_int = 0;
+
+  std::cout << "Please enter an integer year value: ";
+  std::cin >> input_int;
+ 
+  if (std::to_string(input_int).length() !== 4) {
+
+    std::cout << "Not a valid four-digit year";
+   
+  }
+  else if (input_int % 4 == 0 && input_int % 100 != 0 || input_int % 400 == 0) {
+
+    std::cout << input_int;
+    std::cout << " falls on a leap year.\n";
+   
+  }
+  else {
+   
+    std::cout << input_int;
+    std::cout << " is not a leap year.\n";
+   
+  }
+}
