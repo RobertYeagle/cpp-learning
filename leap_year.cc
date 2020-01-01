33f0cc778f29b2f1aa4e2554b1bc4a8d31dd91c4
Robert Yeagle <robertsyeagle@gmail.com>
2020-01-01 14:04:37 -0800

    Adding updates and comments

--- leap_year.cc
+++ leap_year.cc
@@ -4,14 +4,17 @@ int main() {
  
   int input_int = 0;
 
+  // Get user input.
   std::cout << "Please enter an integer year value: ";
   std::cin >> input_int;
  
+  // Check user input if valid.
   if (std::to_string(input_int).length() !== 4) {
 
     std::cout << "Not a valid four-digit year";
    
   }
+  // Determine if leap year.
   else if (input_int % 4 == 0 && input_int % 100 != 0 || input_int % 400 == 0) {
 
     std::cout << input_int;
